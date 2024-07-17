ccfbe588a8016b6d86b828b774c03cdf3cc4afc0
Ankit Gupta <ankit.gupta@intercom.io>
2024-07-17 22:28:52 +0100

    Add docker compose and mongo compose files;

--- app-service/src/logger/winston.js
+++ app-service/src/logger/winston.js
@@ -14,18 +14,18 @@ const newdate = `${date.getDate()}-${date.getMonth()}-${date.getFullYear()}`
 const options = {
   info: {
     level: "info",
-    dirname: "logs/combined",
+    dirname: "logs",
     json: true,
     handleExceptions: true,
     datePattern: "YYYY-MM-DD-HH",
-    filename: `combined-${newdate}.log`,
+    filename: `combined-outerr.log`,
   },
   error: {
     level: "error",
-    dirname: "logs/error",
+    dirname: "logs",
     json: true,
     handleExceptions: true,
-    filename: `error-${newdate}.log`,
+    filename: `err.log`,
   },
   console: {
     level: "debug",
